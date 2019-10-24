804aa48638fce463ed8fd55003d9b013ec6846a0
longjianqu <56524554+longjianqu@users.noreply.github.com>
2019-10-24 15:41:34 +0800

    Create 2019.10.24.php

---
+++ 2019.10.24.php
@@ -0,0 +1,9 @@
+<form action="http://www.baidu.com/baidu" target="_blank">
+                        <input id="kw" type="text" baiduSug="1" size="30" name="word">
+                        <input type="subimt" name="百度">
+                    </form>
+
+                    <!--百度搜索的提示-->
+                    <script charset="gbk" src="http://www.baidu.com/js/opensug.js">
+                        
+                    </script>
