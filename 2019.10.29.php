69849b29b0c4b39e1fdde600af9244e44d2077e0
longjianqu <56524554+longjianqu@users.noreply.github.com>
2019-10-29 09:56:19 +0800

    Create 2019.10.29.php

---
+++ 2019.10.29.php
@@ -0,0 +1,72 @@
+	<style type="text/css">
+		.main-promote .col-6{
+			background:#dedede;
+		}
+		.mani-promote .cat{
+			background: #6e6568;
+			color: #fff;
+		}
+		.mani-promote .cat .itim{
+			padding: 16px 20px;
+		}
+		.mani-promote .cat .itim:hover{
+			background: rgba(0,0,0, 0.2);
+		}
+		.mani-promote .info{
+			padding: 10px;
+			background: #fff;
+			color: #888;
+			box-shadow: 0 1px 2px rgba(0,0,0, 0.1)
+		}
+		.main-promote .info .avatar{
+			background: #aaa;
+			width: 50px;
+			height: 50px;
+			float: left;
+			border-radius:50%;
+			margin-right: 10px;
+		}
+
+	</style>
+<body>
+{hkcms:template file="content/header.php"/}
+
+<!-- Banner -->
+<div class="main-promte clearfix">
+	<div class="container">
+		<div class="item">1</div>
+		<div class="item">2</div>
+		<div class="item">3</div>
+		<div class="item">4</div>
+		<div class="item">5</div>
+		<div class="item">6</div>
+		<div class="item">7</div>
+		<div class="item">8</div>
+		<div class="item">9</div>
+	</div>
+	<div class="col-6">
+		<div class="silder">
+			<img src="images/silder01.png" alt="轮播图">
+		</div>
+		<div class="sub-promote">
+			<div class="col-5">
+				<img src="images/001.png"  alt="广告图1">
+			</div>
+			<div class="col-5">
+				<img src="images/002.png" alt="广告图2">
+			</div>		
+	</div>
+</div>
+<div class="col-2 info">
+	<div class="auth claearfix">
+		<div class="avatar"></div>
+		你好~~欢迎剁手~~~
+	</div>
+	<div class="anno">
+		<div class="title">公告</div>
+		<div class="content">
+			我们是侧边栏
+		</div>
+	 </div> 
+  </div>
+</div>
